a6a4966d43b3a8aae95137a14144960c4aaa0cd3
Vitaly <68599754+ALIWAY-VK@users.noreply.github.com>
2021-07-08 17:43:31 +0300

    7#Декоративные элементы на главной странице
    Декоративные элементы на главной странице

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const searchBtn = document.querySelector('.form-search-link');
+const form = document.querySelector('.form');
+const formInputs = document.querySelectorAll('.form input');
+
+if (form) {
+    const checkFormValidity = () => {
+        let flag = true;
+        formInputs.forEach((input) => {
+            if (!input.value) {
+                flag = false;
+            }
+        });
+        return flag;
+    }
+
+    form.classList.remove('is-open');
+    searchBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        form.classList.toggle('is-open');
+    });
+
+    form.addEventListener('submit', (e) => {
+        if (!checkFormValidity()) {
+            e.preventDefault();
+            form.classList.remove('error');
+            form.offsetWidth = form.offsetWidth;
+            form.classList.add('error');
+        }
+    });
+}
